6971a23fa43c1256e7cac78ed69af2f45abcdcc0
Hoppy <251205162@ccmailg.meijo-u.ac.jp>
2026-07-21 15:26:05 +0900

    ミニゲームの内容を修正しました
    ボタンの機能を追加・修正しました．

--- js/minigame.js
+++ js/minigame.js
@@ -1,3 +1,4 @@
+// ハンバーガーメニューの関数
 window.addEventListener('load', function () {
     var $button = this.document.querySelector('.toggle-menu-button');
     var $menu = this.document.querySelector('.header-site-menu');
@@ -12,35 +13,77 @@ window.addEventListener('load', function () {
     });
 });
 
-function makeAnswer(value){
+// 変数の初期化
+var nums = [];
+var answer = "";
+var text_result = "";
+
+// 解答を生成する関数
+function makeAnswer(value,btn,type){
+    // 文字列として加える
     answer += value;
-
     document.getElementById("answer").textContent = answer;
+
+    // 数字を使用不可能に
+    if (type == 0){
+        btn.disabled = true;
+    }
 }
 
+// 解答の(一時)計算
+function calclate(){
+    try {
+        answer = Function('"use strict"; return (' + answer + ')')();
+        answer = String(answer);
+        document.getElementById("answer").textContent = answer;
+    } catch (e) {
+        text_result = "式が正しくありません";
+        document.getElementById("result").textContent = text_result;
+    }
+}
+
+// 解答をクリアする関数
 function clearAnswer(){
-    answer += "";
+    // 解答を初期状態に直す
+    answer = "";
+    text_result = "";
 
+    // 表記も初期状態に直す
     document.getElementById("answer").textContent = answer;
+    document.getElementById("result").textContent = text_result;
+
+    // 数字を全て使用可能にする
+    document.querySelectorAll('.num').forEach(btn => {btn.disabled = false;});
 }
 
-function judgeAnswer(answer){
-    
+// 解答を判定する関数
+function judgeAnswer(){
+    // 文字列を式に変換
+    answer = Function('"use strict"; return (' + answer + ')')();
+
+    if (answer == 10){
+        text_result = "正解！";
+        document.getElementById("result").textContent = text_result;
+    }else{
+        text_result = "違います，，";
+        document.getElementById("result").textContent = text_result;
+    }
 }
 
+// 問題生成の関数
 function makeProblem(){
-    let nums = [];
-    nums = out_nums(10, nums);
+    nums = [];
+    answer = "";
+    clearAnswer();
 
-    nums.sort(() => Math.random() - 0.5);
+    nums = out_nums(10, nums);
 
     document.getElementById("numbers").textContent  = nums.join(" ");
 }
 
 
-// 少なくとも一つは解のある問題を作る関数
-function out_nums(goal,nums){
-    // numは出力する配列
+// 少なくとも一つは解のある問題を作り，配列で返す関数
+function out_nums(goal,nums){   // numは出力する配列
 
     // 配列の大きさが3以上の時，残りの数を数字の列に加える
     if (nums.length >= 3)  {
